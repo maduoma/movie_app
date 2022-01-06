fd57aaa4296792a62521c7ce66cd76ea3797400e
Maduoma <18477889+maduoma@users.noreply.github.com>
2022-01-06 18:33:34 +0100

    Final Version

--- lib/main.dart
+++ lib/main.dart
@@ -76,6 +76,32 @@ class _MoviesListingState extends State<MoviesListing> {
     //fetchMovies();
 
     return Scaffold(
+      //AppBar
+      appBar: AppBar(
+        //Adding background color to AppBar
+        backgroundColor: Colors.purple,
+        //backgroundColor: Colors.white,
+
+        //Adding back arrow for leading property.
+        //Back arrow appears at the top-left of AppBar
+        leading: const Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+        actions: <Widget>[
+          //Adding Star icon for favorting the contact.
+          //This IconButton responds to user's tap,
+          //and prints the message.
+          IconButton(
+            icon: const Icon(Icons.star_border),
+            color: Colors.white,
+            onPressed: () {
+              print("Contact is starred");
+            },
+          ),
+        ],
+      ),
+
       body: ListView.builder(
         //Calculating list size
         itemCount: movies == null ? 0 : movies.length,
@@ -117,39 +143,40 @@ class MovieTile extends StatelessWidget {
           //Resizing image poster based on the screen size whenever the image's path is not null.
           movies[index].posterPath != null
               ? Container(
-            //Making image's width to half of the given screen size
-            width: MediaQuery.of(context).size.width / 2,
+                  //Making image's width to half of the given screen size
+                  width: MediaQuery.of(context).size.width / 2,
 
-            //Making image's height to one fourth of the given screen size
-            height: MediaQuery.of(context).size.height / 4,
+                  //Making image's height to one fourth of the given screen size
+                  height: MediaQuery.of(context).size.height / 4,
 
-            //Making image box visually appealing by dropping shadow
-            decoration: BoxDecoration(
-              //Making image box slightly curved
-              borderRadius: BorderRadius.circular(10.0),
-              //Setting box's color to grey
-              color: Colors.grey,
-
-              //Decorating image
-              image: DecorationImage(
-                  image: NetworkImage(MoviesProvider.imagePathPrefix +
-                      movies[index].posterPath),
-                  //Image getting all the available space
-                  fit: BoxFit.cover),
-
-              //Dropping shadow
-              boxShadow: const [
-                BoxShadow(
-                  //grey colored shadow
+                  //Making image box visually appealing by dropping shadow
+                  decoration: BoxDecoration(
+                    //Making image box slightly curved
+                    borderRadius: BorderRadius.circular(10.0),
+                    //Setting box's color to grey
                     color: Colors.grey,
-                    //Applying softening effect
-                    blurRadius: 3.0,
-                    //move 1.0 to right (horizontal), and 3.0 to down (vertical)
-                    offset: Offset(1.0, 3.0)),
-              ],
-            ),
-          )
-              : Container(), //Empty container when image is not available
+
+                    //Decorating image
+                    image: DecorationImage(
+                        image: NetworkImage(MoviesProvider.imagePathPrefix +
+                            movies[index].posterPath),
+                        //Image getting all the available space
+                        fit: BoxFit.cover),
+
+                    //Dropping shadow
+                    boxShadow: const [
+                      BoxShadow(
+                          //grey colored shadow
+                          color: Colors.grey,
+                          //Applying softening effect
+                          blurRadius: 3.0,
+                          //move 1.0 to right (horizontal), and 3.0 to down (vertical)
+                          offset: Offset(1.0, 3.0)),
+                    ],
+                  ),
+                )
+              : Container(),
+          //Empty container when image is not available
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Text(
