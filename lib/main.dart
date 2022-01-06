54461bc6acde6cde7bf590a0d53f767b93754e3f
Maduoma <18477889+maduoma@users.noreply.github.com>
2022-01-06 14:43:54 +0100

    V3 : Version 3

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-//Printing API response
+//Displaying API response in ListView
 void main() => runApp(const MoviesApp());
 
-//MovieApp stateless widget at app level
+//App level widget
 class MoviesApp extends StatelessWidget {
   const MoviesApp({Key? key}) : super(key: key);
 
@@ -15,19 +15,20 @@ class MoviesApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      //Adding movie listing widget as app's home screen
+      //movie listing stateful widget
       home: MoviesListing(),
     );
   }
 }
 
+// A class for anything related to movies
 class MoviesProvider {
   static const String imagePathPrefix = 'https://image.tmdb.org/t/p/w500/';
 
-  //REPLACE: Replace YOUR_API_KEY with your API key
+  //REPLACE: Don't forget to replace with your own API key
   static const apiKey = "8eaf8205d69cf7f36ddd79ceba2ff248";
 
-  //Returning JSON data as Map
+  //Returns JSON formatted response as Map
   static Future<Map> getJson() async {
     const apiEndPoint =
         "http://api.themoviedb.org/3/discover/movie?api_key=$apiKey&sort_by=popularity.desc";
@@ -36,7 +37,7 @@ class MoviesProvider {
   }
 }
 
-//Movie listing widget
+// This class has to be stateful, because the content changes as movies are fetched.
 class MoviesListing extends StatefulWidget {
   const MoviesListing({Key? key}) : super(key: key);
 
@@ -44,32 +45,43 @@ class MoviesListing extends StatefulWidget {
   _MoviesListingState createState() => _MoviesListingState();
 }
 
-//State object
 class _MoviesListingState extends State<MoviesListing> {
   dynamic movies;
 
   fetchMovies() async {
     var data = await MoviesProvider.getJson();
 
-    //Updating movies variable with fresh data
+    //Updating data and requesting to rebuild widget
     setState(() {
+      //storing movie list in `movies` variable
       movies = data['results'];
     });
   }
 
-  //Building main screen
   @override
   Widget build(BuildContext context) {
-    //Calling method to fetch movies
+
+    //Request to fetch movies
     fetchMovies();
 
     return Scaffold(
-      body: SingleChildScrollView(
-        //Displaying JSON response data in plain text on main screen
-        child: movies != null
-            ? Text("TMDB Api response\n $movies")
-            : const Text("Loading api response"),
+      //Rendering movies in ListView
+      body: ListView.builder(
+        // Calculating number of items using `movies` variable
+        itemCount: movies == null ? 0 : movies.length,
+        // Passing widget handle as `context`, and `index` to process one item at a time
+        itemBuilder: (context, index) {
+
+          return Padding(
+            //Adding padding around the list row
+            padding: const EdgeInsets.all(8.0),
+
+            //Displaying title of the movie only for now
+            child: Text(movies[index]["title"]),
+          );
+        },
       ),
+      //ENDS
     );
   }
 }
