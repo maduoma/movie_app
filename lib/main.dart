64e76dc7761d469a9a9281878a86644e190788b2
Maduoma <18477889+maduoma@users.noreply.github.com>
2022-01-06 13:58:57 +0100

    V2 : Version 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,71 @@
-//import 'dart:convert';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-// import 'package:http/http.dart' as http; // @dart=2.9
+//Printing API response
 void main() => runApp(const MoviesApp());
 
-//REPLACE: Replace YOUR_API_KEY with your API key
-const apiKey = "8eaf8205d69cf7f36ddd79ceba2ff248";
-
+//MovieApp stateless widget at app level
 class MoviesApp extends StatelessWidget {
   const MoviesApp({Key? key}) : super(key: key);
-
-  //const MoviesApp({Key key}) : super(key: key); // @dart=2.9
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+
+      //Adding movie listing widget as app's home screen
       home: MoviesListing(),
     );
   }
 }
 
+class MoviesProvider {
+  static const String imagePathPrefix = 'https://image.tmdb.org/t/p/w500/';
+
+  //REPLACE: Replace YOUR_API_KEY with your API key
+  static const apiKey = "8eaf8205d69cf7f36ddd79ceba2ff248";
+
+  //Returning JSON data as Map
+  static Future<Map> getJson() async {
+    const apiEndPoint =
+        "http://api.themoviedb.org/3/discover/movie?api_key=$apiKey&sort_by=popularity.desc";
+    final apiResponse = await http.get(Uri.parse(apiEndPoint));
+    return json.decode(apiResponse.body);
+  }
+}
+
+//Movie listing widget
 class MoviesListing extends StatefulWidget {
   const MoviesListing({Key? key}) : super(key: key);
-
-  //const MoviesListing({Key key}) : super(key: key); // @dart=2.9
 
   @override
   _MoviesListingState createState() => _MoviesListingState();
 }
 
+//State object
 class _MoviesListingState extends State<MoviesListing> {
-  //Variable to hold movies information
-  dynamic movies; //change to dynamic instead of var
+  dynamic movies;
 
-  //NOTE: Method to make http requests
-  static dynamic getJson() async {
-    //URL to fetch movies information
-    const apiEndPoint =
-        "http://api.themoviedb.org/3/discover/movie?api_key=$apiKey&sort_by=popularity.desc";
-    final apiResponse = await http.get(Uri.parse(apiEndPoint));
-    //final apiResponse = await http.get(apiEndPoint); //Deprecated, use //response = await http.get(Uri.parse("api path"));
-    //'Instance of Response'
-    return apiResponse;
-  }
-
-  //Method to fetch movies from network
   fetchMovies() async {
-    //Getting json
-    var data = await getJson();
+    var data = await MoviesProvider.getJson();
 
+    //Updating movies variable with fresh data
     setState(() {
-      movies = data;
+      movies = data['results'];
     });
   }
 
+  //Building main screen
   @override
   Widget build(BuildContext context) {
-    //Fetch movies
+    //Calling method to fetch movies
     fetchMovies();
 
     return Scaffold(
-      //SingleChildScrollView to provide scrolling for flexible data rendering
       body: SingleChildScrollView(
-        //Print API response on screen.
-        //RESULT: At this point only text 'instance of Response' will be printed
+        //Displaying JSON response data in plain text on main screen
         child: movies != null
             ? Text("TMDB Api response\n $movies")
             : const Text("Loading api response"),
@@ -74,5 +73,3 @@ class _MoviesListingState extends State<MoviesListing> {
     );
   }
 }
-
-// return with: flutter run --no-sound-null-safety in your Terminal of Android Studio.
